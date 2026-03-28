37544739fbc951a42c23bb8127728a97780f20aa
Stefan Lekrem <lekrem@icloud.com>
2026-03-28 15:41:44 +0100

    refactor: replace minimal API endpoints with API controllers

--- src/ghGPT.Api/Program.cs
+++ src/ghGPT.Api/Program.cs
@@ -1,9 +1,9 @@
-using ghGPT.Api.Endpoints;
 using ghGPT.Api.Hubs;
 using ghGPT.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
@@ -17,8 +17,8 @@ app.UseSwaggerUI();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.MapControllers();
 app.MapHub<RepositoryHub>("/hubs/repository");
-app.MapRepositoryEndpoints();
 
 app.Run();
 
