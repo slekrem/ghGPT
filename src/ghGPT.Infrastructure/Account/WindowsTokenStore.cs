c9115e015daa011335db2e7aa872a5abe135ac6a
Stefan <7773913+slekrem@users.noreply.github.com>
2026-04-06 09:59:17 +0200

    refactor(account): Remove platform-specific token storage implementations

--- src/ghGPT.Infrastructure/Account/WindowsTokenStore.cs
+++
@@ -1,8 +0,0 @@
-namespace ghGPT.Infrastructure.Account;
-
-internal sealed class WindowsTokenStore : ITokenStore
-{
-    public void Save(string token) => WindowsCredentialManager.Save(token);
-    public string? Load() => WindowsCredentialManager.Load();
-    public void Delete() => WindowsCredentialManager.Delete();
-}
