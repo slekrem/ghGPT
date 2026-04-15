e0de114d03b35de9c6397ad496fcabef78472649
Stefan Lekrem <lekrem@icloud.com>
2026-04-15 15:36:17 +0200

    feat(git.process): Repository-Client in GitClient implementieren

--- src/Git.Process/GitClient.cs
+++ src/Git.Process/GitClient.cs
@@ -1,11 +1,19 @@
 using Git.Process.Abstractions;
+using Git.Process.Repository;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Git.Process;
 
 public class GitClient
 {
-    internal GitClient(IGitRunner runner) { }
+    public IGitRepositoryClient Repository { get; }
+
+    public GitClient() : this(new GitRunner()) { }
+
+    internal GitClient(IGitRunner runner)
+    {
+        Repository = new GitRepositoryClient(runner);
+    }
 }
 
 public static class GitClientServiceCollectionExtensions
@@ -13,6 +21,7 @@ public static class GitClientServiceCollectionExtensions
     public static IServiceCollection AddGitProcess(this IServiceCollection services)
     {
         services.AddSingleton<IGitRunner, GitRunner>();
+        services.AddSingleton<IGitRepositoryClient, GitRepositoryClient>();
         services.AddSingleton<GitClient>();
         return services;
     }
